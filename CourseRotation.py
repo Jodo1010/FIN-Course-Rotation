ebcf6a4518c8716411a35910d06be81ff9b4e7e1
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 15:40:01 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-# Initialize state for each course and semester if not already initialized
+# Initialize state for each required course and semester if not already initialized
 required_courses = ['FIN 354', 'FIN 355', 'FIN 358', 'FIN 359', 'FIN 450W']
 restricted_electives_fall = ['FIN 451', 'FIN 456']
 restricted_electives_winter = ['FIN 453', 'FIN 455', 'FIN 457']
 
-for course in required_courses + restricted_electives_fall + restricted_electives_winter:
+for course in required_courses:
     if f"{course}_fall" not in st.session_state:
         st.session_state[f"{course}_fall"] = 'In-person'
     if f"{course}_winter" not in st.session_state:
@@ -21,30 +21,31 @@ st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
 st.subheader('Required Courses')
 for course in required_courses:
     col1, col2 = st.columns(2)
-
+    
     # Fall semester dropdown in the left column
     with col1:
         fall_option = st.selectbox(
             f"{course} (Fall):", ['In-person', 'Fully Online'], key=f"{course}_fall"
         )
-
+    
     # Display Winter semester options in the right column based on Fall selection
     with col2:
         winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
         st.write(f"{course} (Winter) options: {winter_options}")
 
 # Restricted Electives
-st.subheader('Restricted Electives (Fall)')
-for course in restricted_electives_fall:
-    fall_option = st.selectbox(
-        f"{course} (Fall):", ['In-person', 'Fully Online'], key=f"{course}_fall"
-    )
+st.subheader('Restricted Electives')
+col1, col2 = st.columns(2)
 
-st.subheader('Restricted Electives (Winter)')
-for course in restricted_electives_winter:
-    winter_option = st.selectbox(
-        f"{course} (Winter):", ['In-person', 'Fully Online'], key=f"{course}_winter"
-    )
+with col1:
+    st.write("Fall")
+    for course in restricted_electives_fall:
+        st.write(f"{course}: In-Person or Fully Online")
+
+with col2:
+    st.write("Winter")
+    for course in restricted_electives_winter:
+        st.write(f"{course}: In-Person or Fully Online")
 
 # Notes Section
 st.text_area('Notes:', '')
