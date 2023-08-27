497656d75d8a5f108f0619347e05945fe3279a49
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 16:41:10 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -4,8 +4,17 @@ import streamlit as st
 required_courses = ['FIN 354', 'FIN 355', 'FIN 358', 'FIN 359', 'FIN 450W']
 restricted_electives_fall = ['FIN 451', 'FIN 456']
 restricted_electives_winter = ['FIN 453', 'FIN 455', 'FIN 457']
+required_graduate_courses = ['FIN 502', 'FIN 620']
 
 for course in required_courses:
+    if f"{course}_fall" not in st.session_state:
+        st.session_state[f"{course}_fall"] = 'In-person'
+    if f"{course}_winter" not in st.session_state:
+        st.session_state[f"{course}_winter"] = 'In-person'
+
+# Initialize state for each required graduate course and semester if not already initialized
+
+for course in required_graduate_courses:
     if f"{course}_fall" not in st.session_state:
         st.session_state[f"{course}_fall"] = 'In-person'
     if f"{course}_winter" not in st.session_state:
@@ -61,6 +70,28 @@ with col2:
 
     for course in restricted_electives_winter:
         st.markdown(f"<div style='text-align: right'>{course}: In-Person or Fully Online</div>", unsafe_allow_html=True)
+
+# Required Graduate Courses
+st.subheader('Required Graduate Courses')
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("<div style='text-align: left'><strong>Fall</strong></div>", unsafe_allow_html=True)
+with col2:
+    st.markdown("<div style='text-align: right'><strong>Winter</strong></div>", unsafe_allow_html=True)
+
+for course in required_graduate_courses:
+    col1, col2 = st.columns(2)
+
+    # Fall semester dropdown in the left column
+    with col1:
+        fall_option = st.selectbox(f"**{course}:**", ['In-person', 'Fully Online'], key=f"{course}_fall")
+
+    # Display Winter semester options in the right column based on Fall selection
+    with col2:
+        st.markdown(f"<div style='text-align: right'>**{course}:**</div>", unsafe_allow_html=True)
+        winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
+        st.markdown(f"<div style='text-align: right'>**{winter_options}**</div>", unsafe_allow_html=True)
+
 
 # Notes Section
 st.text_area('Notes:', '')
