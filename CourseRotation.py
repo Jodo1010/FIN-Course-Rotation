aa1d43a823fe5c00a34c992eb7a7978eabc45835
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 16:15:20 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -21,41 +21,38 @@ st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
 st.subheader('Required Courses')
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("**Fall**")  # Large and bold text
+    st.markdown("**## Fall**")
 with col2:
-    st.markdown("**Winter**")  # Large and bold text
+    st.markdown("<div style='text-align: right'>**## Winter**</div>", unsafe_allow_html=True)
 
 for course in required_courses:
     col1, col2 = st.columns(2)
 
     # Fall semester dropdown in the left column
     with col1:
-        fall_option = st.selectbox(f"{course}:", ['In-person', 'Fully Online'], key=f"{course}_fall")  
+        fall_option = st.selectbox(f"**{course} (Fall):**", ['In-person', 'Fully Online'], key=f"{course}_fall")
 
     # Display Winter semester options in the right column based on Fall selection
     with col2:
-        st.markdown(f"{course}:")  
+        st.markdown(f"<div style='text-align: right'>**{course} options:**</div>", unsafe_allow_html=True)
         winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
-        st.markdown(f"**{winter_options}**")  # Highlighting the output
+        st.markdown(f"<div style='text-align: right'>**{winter_options}**</div>", unsafe_allow_html=True)
 
 # Restricted Electives
 st.subheader('Restricted Electives')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("**## Fall**")  # Large and bold text
-    for course in restricted_electives_fall:
-        st.text(f"{course}: In-Person or Fully Online")
-
+    st.markdown("**## Fall**")
 with col2:
-    st.markdown("**## Winter**")  # Large and bold text
+    st.markdown("<div style='text-align: right'>**## Winter**</div>", unsafe_allow_html=True)
+
     for course in restricted_electives_winter:
-        st.text(f"{course}: In-Person or Fully Online")
+        st.markdown(f"<div style='text-align: right'>{course}: In-Person or Fully Online</div>", unsafe_allow_html=True)
 
 # Notes Section
 st.text_area('Notes:', '')
 
 # Validate and Submit Button
 if st.button('Submit'):
-    # Validation logic here
     st.write('Submitted Successfully')
