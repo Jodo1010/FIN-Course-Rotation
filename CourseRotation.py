deaa1b68fe887fd30e84090e72c1cfc949c89d1f
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 10:54:47 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -1,45 +1,35 @@
 import streamlit as st
 
-# Initialize state variables
-if 'required_course_modality_fall' not in st.session_state:
-    st.session_state.required_course_modality_fall = {}
-if 'required_course_modality_winter' not in st.session_state:
-    st.session_state.required_course_modality_winter = {}
-
-# Initialize lists for courses
+# Initialize state for each course and semester if not already initialized
 required_courses = ['FIN 354', 'FIN 355', 'FIN 358', 'FIN 359', 'FIN 450W']
-restricted_electives_fall = ['FIN 451', 'FIN 456']
-restricted_electives_winter = ['FIN 453', 'FIN 455', 'FIN 457']
+for course in required_courses:
+    if f"{course}_fall" not in st.session_state:
+        st.session_state[f"{course}_fall"] = 'In-person'
+    if f"{course}_winter" not in st.session_state:
+        st.session_state[f"{course}_winter"] = 'In-person'
 
 # Title and Intro
 st.title('Course Rotation Schedule')
 
 # Academic Year Dropdown
-academic_year = st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
+st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
 
 # Required Courses
 st.subheader('Required Courses')
 for course in required_courses:
     # Fall semester dropdown
-    st.session_state.required_course_modality_fall[course] = st.selectbox(
+    fall_option = st.selectbox(
         f"{course} (Fall):", ['In-person', 'Fully Online'], key=f"{course}_fall"
     )
-
+    
     # Winter semester dropdown with conditional options
-    winter_options = ['In-person', 'Fully Online'] if st.session_state.required_course_modality_fall[course] == 'In-person' else ['In-person']
-    st.session_state.required_course_modality_winter[course] = st.selectbox(
+    winter_options = ['In-person', 'Fully Online'] if fall_option == 'In-person' else ['In-person']
+    winter_option = st.selectbox(
         f"{course} (Winter):", options=winter_options, key=f"{course}_winter"
     )
 
-# Restricted Electives
-st.subheader('Restricted Electives (Fall)')
-restricted_elective_modality_fall = {course: st.selectbox(f"{course} (Fall):", ['In-person', 'Fully Online']) for course in restricted_electives_fall}
-
-st.subheader('Restricted Electives (Winter)')
-restricted_elective_modality_winter = {course: st.selectbox(f"{course} (Winter):", ['In-person', 'Fully Online']) for course in restricted_electives_winter}
-
 # Notes Section
-notes = st.text_area('Notes:', '')
+st.text_area('Notes:', '')
 
 # Validate and Submit Button
 if st.button('Submit'):
