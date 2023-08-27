d4400954d4e0c3b42eaafce9c51f4c9c12b6f969
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 17:10:31 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -42,16 +42,16 @@ with col2:
 for course in required_courses:
     col1, col2 = st.columns(2)
 
+    # Update Fall options if Winter option is selected before Fall
+    if st.session_state[f"{course}_winter"] == 'Fully Online':
+       st.session_state[f"{course}_fall"] = 'In-person'
+
     # Fall semester dropdown in the left column
     with col1:
         fall_option = st.selectbox(f"**{course}:**", ['In-person', 'Fully Online'], key=f"{course}_fall")
 
     # Display Winter semester options in the right column based on Fall selection
-    # with col2:
-    #     # st.markdown(f"<div style='text-align: right'><strong>{course}:<strong></div>", unsafe_allow_html=True)
-    #     # winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
-    #     # st.markdown(f"<div style='text-align: right'>{winter_options}</div>", unsafe_allow_html=True)
-    with col2:
+      with col2:
         winter_options = ['In-person', 'Fully Online'] if fall_option == 'In-person' else ['In-person']
         winter_option = st.selectbox(f"**{course}:**", winter_options, key=f"{course}_winter")
 
@@ -76,15 +76,16 @@ with col2:
 for course in required_graduate_courses:
     col1, col2 = st.columns(2)
 
+    # Update Fall options if Winter option is selected before Fall
+    if st.session_state[f"{course}_winter"] == 'Fully Online':
+       st.session_state[f"{course}_fall"] = 'In-person'
+
     # Fall semester dropdown in the left column
     with col1:
         fall_option = st.selectbox(f"**{course}:**", ['In-person', 'Fully Online'], key=f"{course}_fall")
 
     # Display Winter semester options in the right column based on Fall selection
-    # with col2:
-        # st.markdown(f"<div style='text-align: right'><strong>{course}:<strong></div>", unsafe_allow_html=True)
-        # winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
-        # st.markdown(f"<div style='text-align: right'>{winter_options}</div>", unsafe_allow_html=True)
+
     with col2:
         winter_options = ['In-person', 'Fully Online'] if fall_option == 'In-person' else ['In-person']
         winter_option = st.selectbox(f"**{course}:**", winter_options, key=f"{course}_winter")
