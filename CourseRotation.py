429076551e80d04cbe9c20c1af9f1f36d3e7f29b
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 16:20:39 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -21,9 +21,9 @@ st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
 st.subheader('Required Courses')
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("**## Fall**")
+    st.markdown("**Fall**")
 with col2:
-    st.markdown("<div style='text-align: right'>**## Winter**</div>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: right'><strong>Winter</strong></div>", unsafe_allow_html=True)
 
 for course in required_courses:
     col1, col2 = st.columns(2)
@@ -48,7 +48,7 @@ with col1:
         st.text(f"{course}: In-Person or Fully Online")
         
 with col2:
-    st.markdown("<div style='text-align: right'>** Winter**</div>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: right'><strong>Winter</strong></div>", unsafe_allow_html=True)
 
     for course in restricted_electives_winter:
         st.markdown(f"<div style='text-align: right'>{course}: In-Person or Fully Online</div>", unsafe_allow_html=True)
