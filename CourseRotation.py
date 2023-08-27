a11d5cbb3ae58609f6d0e4ecd61b432d0e432e7f
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 16:08:01 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -30,12 +30,12 @@ for course in required_courses:
 
     # Fall semester dropdown in the left column
     with col1:
-        fall_option = st.selectbox(f"{course} (Fall):", ['In-person', 'Fully Online'], key=f"{course}_fall")
+        fall_option = st.selectbox(f"{course}:", ['In-person', 'Fully Online'], key=f"{course}_fall")
 
     # Display Winter semester options in the right column based on Fall selection
     with col2:
         winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
-        st.write(f"{course} options:")
+        st.write(f"{course}:")
         st.markdown(f"**{winter_options}**")  # Highlighting the output
 
 # Restricted Electives
