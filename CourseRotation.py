fea6b18b1eb8934dd5818193dc8fc1edd68e151b
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2023-08-27 16:13:09 -0400

    CourseRotation.py

--- CourseRotation.py
+++ CourseRotation.py
@@ -21,20 +21,20 @@ st.selectbox('Select Academic Year:', ['2023-2024', '2024-2025', '2025-2026'])
 st.subheader('Required Courses')
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("**## Fall**")  # Large and bold text
+    st.markdown("**Fall**")  # Large and bold text
 with col2:
-    st.markdown("**## Winter**")  # Large and bold text
+    st.markdown("**Winter**")  # Large and bold text
 
 for course in required_courses:
     col1, col2 = st.columns(2)
 
     # Fall semester dropdown in the left column
     with col1:
-        fall_option = st.selectbox(f"**{course} (Fall):**", ['In-person', 'Fully Online'], key=f"{course}_fall")  # Bold text
+        fall_option = st.selectbox(f"{course}:", ['In-person', 'Fully Online'], key=f"{course}_fall")  
 
     # Display Winter semester options in the right column based on Fall selection
     with col2:
-        st.markdown(f"**{course} options:**")  # Bold text
+        st.markdown(f"{course}:")  
         winter_options = 'In-person, Fully Online' if fall_option == 'In-person' else 'In-person'
         st.markdown(f"**{winter_options}**")  # Highlighting the output
 
